9f0e9d72b845d04d55de766ee11bcbe5a89d817a
Romain Fromi <romain.fromi@gmail.com>
2024-09-30 18:23:18 +0200

    Setup building tiles in rules

--- rules/src/ArchitectsOfAmytisSetup.ts
+++ rules/src/ArchitectsOfAmytisSetup.ts
@@ -1,6 +1,7 @@
 import { MaterialGameSetup } from '@gamepark/rules-api'
 import { ArchitectsOfAmytisOptions } from './ArchitectsOfAmytisOptions'
 import { ArchitectsOfAmytisRules } from './ArchitectsOfAmytisRules'
+import { buildings } from './material/Building'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
 import { PlayerColor } from './PlayerColor'
@@ -13,6 +14,23 @@ export class ArchitectsOfAmytisSetup extends MaterialGameSetup<PlayerColor, Mate
   Rules = ArchitectsOfAmytisRules
 
   setupMaterial(_options: ArchitectsOfAmytisOptions) {
+    this.setupBuildingTiles()
+  }
+
+  setupBuildingTiles() {
+    this.material(MaterialType.BuildingTile).createItems(buildings.concat(buildings).map((building, index) => ({
+      id: building, location: { type: LocationType.MainBoardStackSpace, x: index }
+    })))
+    this.material(MaterialType.BuildingTile).shuffle()
+    const buildingsDeck = this.material(MaterialType.BuildingTile).deck()
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        for (let z = 0; z < 5; z++) {
+          buildingsDeck.dealOne({ type: LocationType.MainBoardStackSpace, x, y, z })
+        }
+      }
+    }
+    buildingsDeck.deleteItems()
   }
 
   start() {
---
+++ rules/src/material/Building.ts
@@ -0,0 +1,46 @@
+import { getEnumValues } from '@gamepark/rules-api'
+
+export enum BuildingType {
+  Garden = 1,
+  Market,
+  Wall,
+  Palace,
+  Residence,
+  Theater
+}
+
+export enum BuildingColor {
+  Blue = 1, Green, Orange, Purple
+}
+
+export enum Building {
+  BlueGarden = 11,
+  BlueMarket,
+  BlueWall,
+  BluePalace,
+  BlueResidence,
+  BlueTheater,
+  GreenGarden = 21,
+  GreenMarket,
+  GreenWall,
+  GreenPalace,
+  GreenResidence,
+  GreenTheater,
+  OrangeGarden = 31,
+  OrangeMarket,
+  OrangeWall,
+  OrangePalace,
+  OrangeResidence,
+  OrangeTheater,
+  PurpleGarden = 41,
+  PurpleMarket,
+  PurpleWall,
+  PurplePalace,
+  PurpleResidence,
+  PurpleTheater
+}
+
+export const buildings = getEnumValues(Building)
+
+export const getBuildingType = (building: Building) => building % 10
+export const getBuildingColor = (building: Building) => Math.floor(building / 10)
