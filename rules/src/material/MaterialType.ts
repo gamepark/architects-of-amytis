c52802ffe1c7c12bd4fd796b7a4c53d22f3c5602
Romain Fromi <romain.fromi@gmail.com>
2024-09-30 15:23:16 +0200

    List all material types

--- rules/src/material/MaterialType.ts
+++ rules/src/material/MaterialType.ts
@@ -1,2 +1,12 @@
 export enum MaterialType {
+  MainBoard = 1,
+  FavorBoard,
+  ScoreBoard,
+  PlayerBoard,
+  Architect,
+  Pawn,
+  FirstPlayerCard,
+  BuildingCard,
+  ProjectCard,
+  BuildingTile
 }
