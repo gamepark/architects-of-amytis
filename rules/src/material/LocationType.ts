e8d0ac4e0f037a9029365a56b17cc2134faf9ee7
Romain Fromi <romain.fromi@gmail.com>
2024-09-30 17:19:04 +0200

    Init Location types for the setup

--- rules/src/material/LocationType.ts
+++ rules/src/material/LocationType.ts
@@ -1,2 +1,17 @@
 export enum LocationType {
+  MainBoardSpot = 1,
+  FavorBoardSpot,
+  ScoreBoardSpot,
+  MainBoardStackSpace,
+  ProjectCardsDeck,
+  ProjectCardsDisplay,
+  PlayerBoardSpot,
+  ScoreBoardSpace,
+  ScoreRangeAreaSpace,
+  PlayerArchitectsSupply,
+  PlayerPawnsSupply,
+  FirstPlayerCardSpot,
+  PlayerProjectCardsSpot,
+  PlayerBuildingCardsSpot,
+  PlayerValidatedProjectCardsPile
 }
