fe30e9df6f6862da4bb68f7909909dc98ee9deef
Sergio Cabrera <sergiocabrera0904@gmail.com>
2024-10-30 20:55:18 +0100

    Favor board help

--- app/src/material/FavorBoardDescription.ts
+++ app/src/material/FavorBoardDescription.ts
@@ -1,6 +1,7 @@
 import { LocationType } from '@gamepark/architects-of-amytis/material/LocationType'
 import { BoardDescription } from '@gamepark/react-game'
 import FavorBoard from '../images/boards/FavorBoard.jpg'
+import { FavorBoardHelp } from './help/FavorBoardHelp'
 
 class FavorBoardDescription extends BoardDescription {
   width = 20
@@ -8,6 +9,8 @@ class FavorBoardDescription extends BoardDescription {
   image = FavorBoard
 
   staticItem = { location: { type: LocationType.FavorBoardSpot } }
+
+  help = FavorBoardHelp
 }
 
 export const favorBoardDescription = new FavorBoardDescription()
---
+++ app/src/material/help/FavorBoardHelp.tsx
@@ -0,0 +1,28 @@
+import { css } from "@emotion/react"
+import { MaterialHelpProps } from "@gamepark/react-game"
+import { FC } from "react"
+import { useTranslation } from "react-i18next"
+
+export const FavorBoardHelp: FC<MaterialHelpProps> = () => {
+  const { t } = useTranslation()
+  return (
+    <>
+      <p css={titleCss}>{ t('favor-board.upper-row.label.help') }</p>
+      <ul>
+        <li>{ t('favor-board.space-1.help') }</li>
+        <li>{ t('favor-board.space-2.help') }</li>
+        <li>{ t('favor-board.space-3.help') }</li>
+        <li>{ t('favor-board.space-4.help') }</li>
+        <li>{ t('favor-board.space-5.help') }</li>
+        <li>{ t('favor-board.space-6.help') }</li>
+        <li>{ t('favor-board.space-7.help') }</li>
+      </ul>
+      <p css={titleCss}>{ t('favor-board.bottom-row.label.help') }</p>
+      <p>{ t('favor-board.bottom-row.help') }</p>
+    </>
+  )
+}
+
+const titleCss = css`
+  font-weight: 700;
+`
