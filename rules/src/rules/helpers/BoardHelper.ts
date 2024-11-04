7c8a99580e879e8bfb3f6bd9db5e40bfeba01a90
Romain Fromi <romain.fromi@gmail.com>
2024-11-04 11:25:30 +0100

    Suggestion for a tic-tac-toe algorithm without looping on every combination of 3 architects

--- rules/src/rules/helpers/BoardHelper.ts
+++ rules/src/rules/helpers/BoardHelper.ts
@@ -1,7 +1,8 @@
-import { Material, MaterialGame, MaterialRulesPart } from "@gamepark/rules-api";
-import { MaterialType } from "../../material/MaterialType";
-import { LocationType } from "../../material/LocationType";
-import { Memory } from "../Memory";
+import { Material, MaterialGame, MaterialRulesPart } from '@gamepark/rules-api'
+import { range } from 'lodash'
+import { LocationType } from '../../material/LocationType'
+import { MaterialType } from '../../material/MaterialType'
+import { Memory } from '../Memory'
 
 export class BoardHelper extends MaterialRulesPart {
   constructor(game: MaterialGame, readonly player?: number) {
@@ -11,51 +12,21 @@ export class BoardHelper extends MaterialRulesPart {
   areArchitectsAligned() {
     const architects = this.material(MaterialType.Architect).location(LocationType.MainBoardStackSpace).id(this.player).getItems()
     if (architects.length < 3) {
-      return false; // Not enough for a line
+      return false // Not enough for a line
     }
-  
-    // Verify horizontal, vertical and diagonal alignment
-    for (let i = 0; i < architects.length - 2; i++) {
-      for (let j = i + 1; j < architects.length - 1; j++) {
-        for (let k = j + 1; k < architects.length; k++) {
-          const obj1 = architects[i];
-          const obj2 = architects[j];
-          const obj3 = architects[k];
-  
-          // Horizontal alignment
-          if (obj1.location.y === obj2.location.y && obj2.location.y === obj3.location.y) {
-            return true;
-          }
-  
-          // Vertical alignment
-          if (obj1.location.x === obj2.location.x && obj2.location.x === obj3.location.x) {
-            return true;
-          }
-  
-          // Diagonal alignment
-          if (
-            (obj1.location.x! - obj2.location.x! === obj1.location.y! - obj2.location.y!) && 
-            (obj1.location.x! - obj3.location.x! === obj1.location.y! - obj3.location.y!)
-          ) {
-            return true;
-          }
-  
-          if (
-            (obj1.location.x! - obj2.location.x! === -(obj1.location.y! - obj2.location.y!)) &&
-            (obj1.location.x! - obj3.location.x! === -(obj1.location.y! - obj3.location.y!))
-          ) {
-            return true;
-          }
-        }
-      }
+    const architectsMatrix = range(0, 3).map(_ => range(0, 3).map(_ => false))
+    for (const architect of architects) {
+      architectsMatrix[architect.location.y!][architect.location.x!] = true
     }
-  
-    return false;
+    return architectsMatrix.some(line => line.every(y => y))
+      || range(0, 3).some(y => architectsMatrix.every(line => line[y]))
+      || range(0, 3).every(i => architectsMatrix[i][i])
+      || range(0, 3).every(i => architectsMatrix[i][2 - i])
   }
 
   getVisibleTilesInStack(tilesInBoard: Material) : Material {
     const topTiles: number[] = []
-    
+
     for (let x = 0; x < 3; x++) {
       for (let y = 0; y < 3; y++) {
         const stack = tilesInBoard.location(location => location.x === x && location.y === y)
@@ -100,6 +71,6 @@ export const Corners = [
   {x: 0, y: 0},
   {x: 0, y: 2},
   {x: 2, y: 0},
-  {x: 2, y: 2}  
+  {x: 2, y: 2}
 ]
 
