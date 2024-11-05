5dbcfa3dcb7c8e984f48947f251b48d3206576d7
Romain Fromi <romain.fromi@gmail.com>
2024-11-05 14:51:13 +0100

    Add EndGameScoreHeader

--- app/src/headers/Headers.tsx
+++ app/src/headers/Headers.tsx
@@ -1,12 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { RuleId } from '@gamepark/architects-of-amytis/rules/RuleId'
 import { ComponentType } from 'react'
-import { RetrieveArchitectsHeader } from './RetrieveArchitectsHeader'
-import { PlaceBuildingTileHeader } from './PlaceBuildingTileHeader'
-import { SelectProjectCardHeader } from './SelectProjectCardHeader'
-import { ClaimKingsFavorHeader } from './ClaimKingsFavorHeader'
+import { EndGameScoreHeader } from './EndGameScoreHeader'
 import { CheckProjectsHeader } from './CheckProjectsHeader'
 import { ChooseBuildingTileHeader } from './ChooseBuildingTileHeader'
+import { ClaimKingsFavorHeader } from './ClaimKingsFavorHeader'
+import { PlaceBuildingTileHeader } from './PlaceBuildingTileHeader'
+import { RetrieveArchitectsHeader } from './RetrieveArchitectsHeader'
+import { SelectProjectCardHeader } from './SelectProjectCardHeader'
 
 export const Headers: Partial<Record<RuleId, ComponentType>> = {
   [RuleId.RetrieveArchitects]: RetrieveArchitectsHeader,
@@ -14,5 +15,6 @@ export const Headers: Partial<Record<RuleId, ComponentType>> = {
   [RuleId.PlaceBuildingTile]: PlaceBuildingTileHeader,
   [RuleId.SelectProjectCard]: SelectProjectCardHeader,
   [RuleId.CheckProjects]: CheckProjectsHeader,
-  [RuleId.ClaimKingsFavor]: ClaimKingsFavorHeader
+  [RuleId.ClaimKingsFavor]: ClaimKingsFavorHeader,
+  [RuleId.EndGameScore]: EndGameScoreHeader
 }
