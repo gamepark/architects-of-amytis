ba9cfe92c283a6d8ba4c8fb9ce02a7444cfb6106
Romain Fromi <romain.fromi@gmail.com>
2024-11-06 14:10:57 +0100

    Fix ScoreBoardHelp for spectators

--- app/src/material/help/ScoreBoardHelp.tsx
+++ app/src/material/help/ScoreBoardHelp.tsx
@@ -1,32 +1,32 @@
 /** @jsxImportSource @emotion/react */
 
-import { ArchitectsOfAmytisRules } from "@gamepark/architects-of-amytis/ArchitectsOfAmytisRules"
-import { Memory } from "@gamepark/architects-of-amytis/rules/Memory"
-import { MaterialHelpProps, usePlayerId, usePlayerName, useRules } from "@gamepark/react-game"
-import { FC } from "react"
-import { Trans, useTranslation } from "react-i18next"
+import { ArchitectsOfAmytisRules } from '@gamepark/architects-of-amytis/ArchitectsOfAmytisRules'
+import { PlayerColor } from '@gamepark/architects-of-amytis/PlayerColor'
+import { Memory } from '@gamepark/architects-of-amytis/rules/Memory'
+import { MaterialHelpProps, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { FC } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
 
 export const ScoreBoardHelp: FC<MaterialHelpProps> = () => {
   const { t } = useTranslation()
   const rules = useRules<ArchitectsOfAmytisRules>()!
-  const player = usePlayerId()
-  const otherPlayer = rules.players[0] === player ? rules.players[1] : rules.players[0]
-  
+  const me = usePlayerId()
+
   return (
     <>
       <h2>{t('score-board')}</h2>
-      <p>
-        <Trans 
-          defaults="score.you"
-          values={{score: rules.remind(Memory.Score, player)}}
-        ></Trans>
-      </p>
-      <p>
-        <Trans 
-          defaults="score.player"
-          values={{player: usePlayerName(otherPlayer), score: rules.remind(Memory.Score, otherPlayer)}}
-        ></Trans>
-      </p>
+      {rules.players.map(player =>
+        <p key={player}>
+          {player === me ?
+            <Trans defaults="score.you" values={{ score: rules.remind(Memory.Score, player) }}/>
+            : <PlayerScoreText playerColor={player} score={rules.remind(Memory.Score, player)}/>}
+        </p>
+      )}
     </>
   )
+}
+
+const PlayerScoreText = ({ playerColor, score }: { playerColor: PlayerColor, score: number }) => {
+  const player = usePlayerName(playerColor)
+  return <Trans defaults="score.player" values={{ player, score }}/>
 }
