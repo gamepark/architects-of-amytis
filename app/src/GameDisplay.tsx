cef34a6a6d3c9ef8f9f0648f01828b5e1e957502
Romain Fromi <romain.fromi@gmail.com>
2024-11-21 14:16:41 +0100

    Inform players when game over is triggered

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -1,7 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { GameTable, GameTableNavigation } from '@gamepark/react-game'
-import { FC } from 'react'
+import { ArchitectsOfAmytisRules } from '@gamepark/architects-of-amytis/ArchitectsOfAmytisRules'
+import { CheckEndGameRule } from '@gamepark/architects-of-amytis/rules/CheckEndGameRule'
+import { GameTable, GameTableNavigation, RulesDialog, ThemeButton, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { FC, useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { PlayerPanels } from './panels/PlayerPanels'
 
 type GameDisplayProps = {
@@ -16,9 +19,30 @@ export const GameDisplay: FC<GameDisplayProps> = () => {
       <GameTableNavigation css={navigationCss}/>
       <PlayerPanels/>
     </GameTable>
+    <EndGameInfo/>
   </>
 }
 
 const navigationCss = css`
   top: 50%;
 `
+
+const EndGameInfo = () => {
+  const { t } = useTranslation()
+  const rules = useRules<ArchitectsOfAmytisRules>()
+  const gameOverTriggered = rules && !rules.isOver() && new CheckEndGameRule(rules.game).has2EmptyStacks()
+  const [open, setOpen] = useState(false)
+  const me = usePlayerId()
+  const player = usePlayerName(rules?.players[1])
+  useEffect(() => {
+    if (gameOverTriggered) {
+      setOpen(true)
+    }
+  }, [gameOverTriggered])
+  return <RulesDialog open={open} css={css`font-size: 3em;
+    padding: 1em;`}>
+    <p>{t('game-over.trigger')}</p>
+    <p>{me === rules?.players[1] ? t('game-over.you') : t('game-over.player', { player })}</p>
+    <ThemeButton onClick={() => setOpen(false)}>{t('OK')}</ThemeButton>
+  </RulesDialog>
+}
