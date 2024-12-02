06e3f05c4ff192914e5e790e5fed939ecee1981d
Romain Fromi <romain.fromi@gmail.com>
2024-12-02 16:46:55 +0100

    Simplify ChooseBuildingTileHeader to use it as documentation example

--- app/src/headers/ChooseBuildingTileHeader.tsx
+++ app/src/headers/ChooseBuildingTileHeader.tsx
@@ -1,15 +1,17 @@
 /** @jsxImportSource @emotion/react */
 import { ArchitectsOfAmytisRules } from '@gamepark/architects-of-amytis/ArchitectsOfAmytisRules'
 import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
-import { Trans } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 export const ChooseBuildingTileHeader = () => {
+  const { t } = useTranslation()
   const rules = useRules<ArchitectsOfAmytisRules>()!
   const me = usePlayerId()
   const activePlayer = rules.getActivePlayer()
-  const itsMe = activePlayer === me
   const player = usePlayerName(activePlayer)
-  return (
-    <Trans defaults={itsMe ? 'header.choose-building.you' : 'header.choose-building.player'} values={{ player }}/>
-  )
+  if (activePlayer === me) {
+    return <>{t('header.choose-building.you')}</>
+  } else {
+    return <>{t('header.choose-building.player', { player })}</>
+  }
 }
